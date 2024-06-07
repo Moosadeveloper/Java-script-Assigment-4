52548a6bc5234388d3c78c53edd467aa7acf4b62
Moosadeveloper <janmoosa77@gmail.com>
2024-06-07 19:59:37 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var cityToCheck =prompt("what is your city");
+var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+
+if (cityToCheck === cleanestCities[0]) {
+    alert("It's one of the cleanest cities");
+     }
+     else if (cityToCheck === cleanestCities[1]) {
+     alert("It's one of the cleanest cities");
+    }
+     else if (cityToCheck === cleanestCities[2]) {
+     alert("It's one of the cleanest cities");
+     }
+     else if (cityToCheck === cleanestCities[3]) {
+     alert("It's one of the cleanest cities");
+     }
+     else if (cityToCheck === cleanestCities[4]) {
+     alert("It's one of the cleanest cities");
+     }
+     else {
+    alert("It's not on the list");
+     }
+     
+
+     for (var i = 0; i <= 4; i++) {
+         if (cityToCheck === cleanestCities[i]) {
+         alert("It's one of the cleanest cities");
+         }
+         }
